3ce166dae074eca480ff7e0b0f9d5c51f4bf35a4
Ana Valdemoro <ana_v_r97@hotmail.com>
2025-08-04 20:31:13 +0100

    feat: add brands route to application

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'brands', pathMatch: 'full' },
+  {
+    path: 'brands',
+    loadComponent: () =>
+      import('./features/brand/components/brands/brands.component').then(m => m.BrandsComponent),
+  },
+];
