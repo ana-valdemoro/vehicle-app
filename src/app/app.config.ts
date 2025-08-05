6c795daf0f01c662db78f17684cef37e1d7ddcc8
Ana Valdemoro <ana_v_r97@hotmail.com>
2025-08-05 18:59:41 +0100

    build: install ngrx/effects to the app

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,17 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideEffects } from '@ngrx/effects';
 import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { provideStore } from '@ngrx/store';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideHttpClient(), provideRouter(routes), provideAnimationsAsync(), provideStore()],
+  providers: [
+    provideHttpClient(),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideStore({}),
+    provideEffects(),
+  ],
 };
