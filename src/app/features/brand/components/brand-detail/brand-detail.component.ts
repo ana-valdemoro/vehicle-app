b94e94561eae1af6ae5b8604d47d5c6e9032fe90
Ana Valdemoro <ana_v_r97@hotmail.com>
2025-08-06 20:40:42 +0100

    feat: create brand-detail component

---
+++ src/app/features/brand/components/brand-detail/brand-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-brand-detail',
+  standalone: true,
+  imports: [],
+  templateUrl: './brand-detail.component.html',
+  styleUrl: './brand-detail.component.css'
+})
+export class BrandDetailComponent {
+
+}
