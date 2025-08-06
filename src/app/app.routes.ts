b201a96bf086b52e53635853a3bfc761050790d2
Ana Valdemoro <ana_v_r97@hotmail.com>
2025-08-06 22:38:58 +0100

    feat: add navigation from details page to main page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,19 @@
+import { routes as ROUTES } from './shared/enums/routes';
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'brands', pathMatch: 'full' },
+  { path: '', redirectTo: ROUTES.BRANDS, pathMatch: 'full' },
   {
-    path: 'brands',
+    path: ROUTES.BRANDS,
     loadComponent: () =>
       import('./features/brand/components/brands/brands.component').then(m => m.BrandsComponent),
+  },
+
+  {
+    path: `${ROUTES.BRANDS}/:id`,
+    loadComponent: () =>
+      import('./features/brand/components/brand-detail/brand-detail.component').then(
+        m => m.BrandDetailComponent,
+      ),
   },
 ];
--- src/app/features/brand/components/brand-detail/brand-detail.component.ts
+++ src/app/features/brand/components/brand-detail/brand-detail.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatCard, MatCardContent, MatCardSubtitle, MatCardTitle } from '@angular/material/card';
+
+import { CurrencyPipe } from '@angular/common';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatIconModule } from '@angular/material/icon';
+import { Router } from '@angular/router';
+import { routes } from '../../../../shared/enums/routes';
 
 @Component({
   selector: 'app-brand-detail',
   standalone: true,
-  imports: [],
+  imports: [
+    CurrencyPipe,
+    MatCard,
+    MatCardTitle,
+    MatCardContent,
+    MatCardSubtitle,
+    MatChipsModule,
+    MatIconModule,
+  ],
   templateUrl: './brand-detail.component.html',
-  styleUrl: './brand-detail.component.css'
+  styleUrl: './brand-detail.component.css',
 })
 export class BrandDetailComponent {
+  private router: Router = inject(Router);
+  vehicleCategories = [
+    {
+      type: 'Sedan',
+      models: [
+        { name: 'Camry', year: 2024, price: 25000 },
+        { name: 'Corolla', year: 2024, price: 22000 },
+        { name: 'Avalon', year: 2024, price: 36000 },
+      ],
+    },
+  ];
 
+  onNavigateBack(): void {
+    this.router.navigate([`/${routes.BRANDS}`]);
+  }
 }
