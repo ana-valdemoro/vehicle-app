4f77473b23030d092c54c25f3d29f574bba79b5e
Ana Valdemoro <ana_v_r97@hotmail.com>
2025-08-10 15:09:52 +0100

    refactor: model-resolver to not use effects

--- src/app/features/brand/components/resolvers/vehicle-model-by-brand.resolver.ts
+++ src/app/features/brand/components/resolvers/vehicle-model-by-brand.resolver.ts
@@ -1,18 +1,23 @@
 import { ActivatedRouteSnapshot, Resolve, Router } from '@angular/router';
 import { Injectable, inject } from '@angular/core';
+import { Observable, of } from 'rxjs';
 import { Store, select } from '@ngrx/store';
-import { filter, first, switchMap, take, tap } from 'rxjs/operators';
-import { selectHasModelsByBrand, selectModelsByBrand } from '../../../store/selectors/vehicle-brand.selectors';
+import {
+  changeLoadingVehicleBrand,
+  loadVehicleModelByBrandSuccess,
+} from '../../../store/actions/vehicle-brand.actions';
+import { filter, finalize, switchMap, take, tap } from 'rxjs/operators';
 
+import { BrandService } from '../../services/brand/brand.service';
 import { VehicleModel } from '../../interfaces/vehicle-model';
-import { loadVehicleModelByBrand } from '../../../store/actions/vehicle-brand.actions';
-import { of } from 'rxjs';
 import { routes } from '../../../../shared/enums/routes';
+import { selectHasModelsByBrand } from '../../../store/selectors/vehicle-brand.selectors';
 
 @Injectable({ providedIn: 'root' })
-export class VehicleModelByBrandResolver implements Resolve<VehicleModel[]> {
-  private store: Store = inject(Store);
-  private router: Router = inject(Router);
+export class VehicleModelByBrandResolver implements Resolve<Observable<VehicleModel[]>> {
+  private store = inject(Store);
+  private router = inject(Router);
+  private brandService = inject(BrandService);
 
   resolve(route: ActivatedRouteSnapshot) {
     const brandId = Number(route.paramMap.get('id'));
@@ -20,23 +25,22 @@ export class VehicleModelByBrandResolver implements Resolve<VehicleModel[]> {
       this.router.navigate([`/${routes.BRANDS}`]);
       return of([]);
     }
+
     return this.store.pipe(
-      // 1) Lee una vez el estado actual para estos modelos
       select(selectHasModelsByBrand(brandId)),
-
-      // 2) Si no hay nada, dispara la acción
-      tap(hasModels => {
-        if (!hasModels) {
-          this.store.dispatch(loadVehicleModelByBrand({ brandId }));
-        }
+      take(1),
+      filter(hasModels => !hasModels),
+      tap(() => {
+        this.store.dispatch(changeLoadingVehicleBrand({ loading: true }));
       }),
-
-      // 3) Ahora espera hasta que el selector emita un valor "true-ish"
       switchMap(() =>
-        this.store.pipe(
-          select(selectModelsByBrand(brandId)),
-          filter(models => models && models.length > 0),
-          first(),
+        this.brandService.getModelsByBrand(brandId).pipe(
+          tap(models => {
+            this.store.dispatch(loadVehicleModelByBrandSuccess({ models, brandId }));
+          }),
+          finalize(() => {
+            this.store.dispatch(changeLoadingVehicleBrand({ loading: false }));
+          }),
         ),
       ),
     );
--- src/app/features/store/effects/vehicle-brand.effects.ts
+++ src/app/features/store/effects/vehicle-brand.effects.ts
@@ -1,8 +1,6 @@
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { Injectable, inject } from '@angular/core';
 import {
-  loadVehicleModelByBrand,
-  loadVehicleModelByBrandSuccess,
   loadVehicleTypesByBrand,
   loadVehicleTypesByBrandSuccess,
 } from '../actions/vehicle-brand.actions';
@@ -14,17 +12,6 @@ import { BrandService } from '../../brand/services/brand/brand.service';
 export class VehicleBrandffects {
   private actions$: Actions = inject(Actions);
   private brandService: BrandService = inject(BrandService);
-
-  loadnVehicleModelByBrand$ = createEffect(() =>
-    this.actions$.pipe(
-      ofType(loadVehicleModelByBrand),
-      switchMap(action =>
-        this.brandService
-          .getModelsByBrand(action.brandId)
-          .pipe(map(models => loadVehicleModelByBrandSuccess({ models, brandId: action.brandId }))),
-      ),
-    ),
-  );
 
   loadVehicleTypesByBrand$ = createEffect(() =>
     this.actions$.pipe(
